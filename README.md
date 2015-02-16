a2032eef1bce1cd861334d1977cff972ef8cb8f4
Ron Waldon <jokeyrhyme@gmail.com>
2015-02-17 08:51:06 +1100

    `getCurrentPosition()` implemented, tested, documented

--- README.md
+++ README.md
@@ -12,11 +12,31 @@ Further, it also exposes a `Promise` API for people that prefer them.
 
 ## API
 
-### clonePosition(Position position)
+### clonePosition(position)
 
 - @param {Position} position returned from GeoLocation API
 - @returns {Object} plain JavaScript object with the coordinate information
 - @throws {TypeError} if `position` or `position.coords` are not objects
+
+### setGeoLocation(geolocation)
+
+- @param {GeoLocation} geolocation object that exposes the GeoLocation API
+
+This is useful for testing. We will use the provided object for all requests
+instead of `navigator.geolocation`.
+
+### getCurrentPosition(onSuccess, onError, options)
+
+- @param {Function} onSuccess
+- @param {Function} onError
+- @param {Object} [options]
+- @returns {Promise} when either `Promise` or `$.Deferred` is globally available
+
+Similar API as the W3C standard: http://dev.w3.org/geo/api/spec-source.html#api_description
+One difference is that we mandate the `onError` handler, to enforce good
+practices.
+The other difference is that we return a Promise when such an implementation is
+available.
 
 ## further reading
 
--- geolocation.js
+++ geolocation.js
@@ -1,3 +1,4 @@
+/*globals $:false*/
 // UMD: https://github.com/umdjs/umd/blob/master/returnExports.js
 (function (root, factory) {
   'use strict';
@@ -15,7 +16,24 @@
 }(this, function () {
   'use strict';
 
+  var api;
+
   var module = {
+
+    setGeoLocation: function (geolocation) {
+      api = geolocation;
+    },
+
+    getGeoLocation: function () {
+      if (api && api.getCurrentPosition) {
+        return api;
+      }
+      if (typeof navigator !== 'undefined' && navigator.geolocation &&
+          navigator.geolocation.getCurrentPosition) {
+        return navigator.geolocation;
+      }
+      return false;
+    },
 
     clonePosition: function (position) {
       if (!position || typeof position !== 'object' || !position.coords || typeof position.coords !== 'object') {
@@ -32,6 +50,83 @@
           speed: position.coords.speed
         }
       };
+    },
+
+    DEFAULT_POSITION_OPTIONS: {
+      enableHighAccuracy: true,
+      maximumAge: 0, // fresh results each time
+      timeout: 10 * 1000 // take no longer than 10 seconds
+    },
+
+    POSITION_OPTION_TYPES: {
+      enableHighAccuracy: 'boolean',
+      maximumAge: 'number',
+      timeout: 'number'
+    },
+
+    mergePositionOptions: function (options) {
+      var result;
+      if (!options || typeof options !== 'object') {
+        return module.DEFAULT_POSITION_OPTIONS;
+      }
+      result = {};
+      Object.keys(module.POSITION_OPTION_TYPES).forEach(function (option) {
+        var type = module.POSITION_OPTION_TYPES[option];
+        var value = options[option];
+        if (typeof options[option] === type && (type !== 'number' || !isNaN(value))) {
+          result[option] = options[option];
+        } else {
+          result[option] = module.DEFAULT_POSITION_OPTIONS[option];
+        }
+      });
+      return result;
+    },
+
+    requestCurrentPosition: function (onSuccess, onError, options) {
+      var geolocation = module.getGeoLocation();
+      if (!geolocation) {
+        throw new Error('the current web engine does not support GeoLocation');
+      }
+      if (typeof onSuccess !== 'function') {
+        throw new TypeError('getCurrentPosition(): 1st parameter must be a Function to handle success');
+      }
+      if (typeof onError !== 'function') {
+        throw new TypeError('getCurrentPosition(): 2nd parameter must be a Function to handle error');
+      }
+      options = module.mergePositionOptions(options);
+      return geolocation.getCurrentPosition(function (position) {
+        onSuccess(module.clonePosition(position));
+      }, onError, options);
+    },
+
+    getPromiseConstructor: function () {
+      if (typeof Promise !== 'undefined') {
+        return Promise;
+      }
+      if (typeof $ !== 'undefined' && $.Deferred) {
+        return $.Deferred;
+      }
+      return false;
+    },
+
+    getCurrentPosition: function (onSuccess, onError, options) {
+      var P = module.getPromiseConstructor();
+      if (P) {
+        return new P(function (resolve, reject) {
+          module.requestCurrentPosition(function (position) {
+            if (typeof onSuccess === 'function') {
+              onSuccess(position);
+            }
+            resolve(position);
+          }, function (err) {
+            if (typeof onError === 'function') {
+              onError(err);
+            }
+            reject(err);
+          }, options);
+        });
+      }
+      return module.requestCurrentPosition(onSuccess, onError, options);
     }
 
   };
